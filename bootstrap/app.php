6280e7abb5b3e0890f0d0c1b0a86de62b2ceed2b
Etienne Marais <etienne.marais@olx.com>
2016-02-05 13:06:09 +0200

    Exclude outline from the production build, it is only used for development purposes.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -79,7 +79,10 @@ $app->singleton(
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
-$app->register(\OutlineLaravel\OutlineLaravelServiceProvider::class);
+if (env('APP_ENV') === 'local') {
+    $app->register(\OutlineLaravel\OutlineLaravelServiceProvider::class);
+}
+
 $app->register(\Barryvdh\Cors\LumenServiceProvider::class);
 
 $app->configure('cors');
