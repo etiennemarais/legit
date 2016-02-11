8e0c6fc8a3f8ca2d7326c27e1e8aaafe095bd203
Etienne Marais <etienne.marais@olx.com>
2016-02-11 10:52:52 +0200

    Code send - Added the tests to cover the success and validation errors parts

--- tests/Legit/Code/CodeTest.php
+++ tests/Legit/Code/CodeTest.php
@@ -55,4 +55,47 @@ class CodeTest extends TestCase
             ]);
         $this->assertResponseStatus(400);
     }
+
+    public function testCodeSend_ReturnsPhoneNumberValidationErrors()
+    {
+        $data = [
+            'phone_number' => '8484811811', # bad format
+            'client_user_id' => 1,
+        ];
+
+        $this->post('api/v1/code/send', $data)
+            ->seeJsonEquals([
+                "status" => 406,
+                "message" => "The phone number is not the correct format for ZA,"
+                    . " The phone number must be at least 11 characters.",
+            ]);
+
+        $this->assertResponseStatus(406);
+    }
+
+    public function testCodeSend_ReturnsOnCodeSuccessfullySent()
+    {
+        factory(\Legit\Verification\Verification::class)->create([
+            'phone_number' => '27848118111',
+            'client_user_id' => 1,
+            'verification_status' => 'unverified',
+        ]);
+
+        $data = [
+            'phone_number' => '27848118111',
+            'client_user_id' => 1,
+        ];
+
+        $this->post('api/v1/code/send', $data)
+            ->seeJsonEquals([
+                "status" => 200,
+                "message" => "Successfully sent verification code",
+                "data" => [
+                    'verification_status' => "awaiting verification",
+                    'expires_at' => 'I still need to do this part',
+                ],
+            ]);
+
+        $this->assertResponseStatus(200);
+    }
 }
